e22629cfa473ee1b4c377d6a08d1c2d982bcc805
manu-180 <manumanu97@hotmail.com>
2025-03-14 02:37:23 -0300

    actualizando pagina webbb

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6356f296937abb89c7e57e222c593778
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "8737726fec2cc5d38018df71fae5c9bc",
+"flutter_bootstrap.js": "6ab7b2a5422a8eb9b036f8feaf4c55cd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
